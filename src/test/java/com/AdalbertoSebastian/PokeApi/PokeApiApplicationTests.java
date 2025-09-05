77269ab775b06e4f4a2fbcbb720dee06abfd3000
Adalberto Salinas <>
2025-09-05 14:07:39 -0600

    Creacion del proyecto PokeApi

---
+++ src/test/java/com/AdalbertoSebastian/PokeApi/PokeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdalbertoSebastian.PokeApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
