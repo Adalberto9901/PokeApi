25fc0eb63b9e866c223886e54032f0e4ff2d84e8
digis <digis@ALIEN18>
2025-09-05 16:49:27 -0600

    ML Creacion de entidades

---
+++ src/main/java/com/AdalbertoSebastian/PokeApi/ML/Sprites.java
@@ -0,0 +1,15 @@
+package com.AdalbertoSebastian.PokeApi.ML;
+
+class Sprites {
+
+    private String backdefault;
+
+    public String getBackdefault() {
+        return backdefault;
+    }
+
+    public void setBackdefault(String backdefault) {
+        this.backdefault = backdefault;
+    }
+
+}
